38c56491c3ba05ef98a4ecb594c4b1edd1d70d89
AlonZolfi <alonzolfi44@gmail.com>
2019-03-28 15:22:01 +0200

    added host services

--- src/ViewControllers/Main.java
+++ src/ViewControllers/Main.java
@@ -1,6 +1,8 @@
+package ViewControllers;
+
 import Controller.Controller;
-import ViewControllers.IView;
 import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,10 +10,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Main instance;
+
+    public static Main getInstance(){
+        if (instance ==null)
+            instance = new Main();
+        return instance;
+    }
+
+    public HostServices hostServices = getHostServices();
+
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Controller controller = new Controller();
--- src/ViewControllers/SearchController.java
+++ src/ViewControllers/SearchController.java
@@ -8,7 +8,7 @@ public class SearchController implements IView{
 
     @Override
     public void setCurrentStage(Stage primaryStage) {
-        
+
     }
 
     public void setController(Controller controller) {
