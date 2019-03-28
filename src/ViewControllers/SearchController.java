1f9c5cef034788d524428643379083c4fc6742e9
hilakhadad <hilakhadad@gmail.com>
2019-03-28 15:20:16 +0200

    search controller added fields

--- src/ViewControllers/SearchController.java
+++ src/ViewControllers/SearchController.java
@@ -1,9 +1,27 @@
 package ViewControllers;
 
 import Controller.Controller;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.controlsfx.control.CheckComboBox;
 
 public class SearchController {
+
     private Controller controller;
+    public Button btn_search;
+    public CheckComboBox ccb_years;
+    public CheckComboBox ccb_courses;
+    public CheckComboBox ccb_departments;
+    public TableView tv_results;
+    public TableColumn tc_lecturer;
+    public TableColumn tc_year;
+    public TableColumn tc_type;
+    public TableColumn tc_course;
+    public TableColumn tc_department;
+    public CheckComboBox ccb_tegs;
+
+    
     public void setController(Controller controller) {
         this.controller = controller;
     }
