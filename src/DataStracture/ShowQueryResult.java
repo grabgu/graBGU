41ee9f7296176e647e80d2d6bd6f2f72daa2fb27
hilakhadad <hilakhadad@gmail.com>
2019-03-28 18:51:12 +0200

    ShowResult all done

--- src/DataStracture/ShowQueryResult.java
+++ src/DataStracture/ShowQueryResult.java
@@ -5,26 +5,26 @@ import javafx.beans.property.StringProperty;
 
 public class ShowQueryResult {
 
-    private StringProperty sp_Dipartment;
+    private StringProperty sp_Department;
     private StringProperty sp_Course;
     private StringProperty sp_Type;
     private StringProperty sp_Year;
-    private StringProperty sp_Lectuarer;
+    private StringProperty sp_Lecturer;
     private StringProperty sp_docLink;
 
 
     ShowQueryResult(String docInfo) {
         String [] docInfos= new String [6];
-        sp_Dipartment = new SimpleStringProperty(docInfos[0]);
+        sp_Department = new SimpleStringProperty(docInfos[0]);
         sp_Course = new SimpleStringProperty(docInfos[1]);
         sp_Type = new SimpleStringProperty(docInfos[2]);
         sp_Year = new SimpleStringProperty(docInfos[3]);
-        sp_Lectuarer = new SimpleStringProperty(docInfos[4]);
+        sp_Lecturer = new SimpleStringProperty(docInfos[4]);
         sp_docLink = new SimpleStringProperty(docInfos[5]);
     }
 
     public StringProperty getDipartmentProperty() {
-        return sp_Dipartment;
+        return sp_Department;
     }
 
 
@@ -41,7 +41,7 @@ public class ShowQueryResult {
     }
 
     public StringProperty getLectuarerProperty() {
-        return sp_Lectuarer;
+        return sp_Lecturer;
     }
 
 
