8794c707ca9f26d840052452b0a9c4b9ee63a335
hilakhadad <hilakhadad@gmail.com>
2019-03-28 18:49:35 +0200

    ShowResult all done

--- src/DataStracture/ShowQueryResult.java
+++ src/DataStracture/ShowQueryResult.java
@@ -5,17 +5,48 @@ import javafx.beans.property.StringProperty;
 
 public class ShowQueryResult {
 
-    private StringProperty sp_docName;
+    private StringProperty sp_Dipartment;
+    private StringProperty sp_Course;
+    private StringProperty sp_Type;
+    private StringProperty sp_Year;
+    private StringProperty sp_Lectuarer;
+    private StringProperty sp_docLink;
 
-    ShowQueryResult(String docName) {
-        sp_docName = new SimpleStringProperty(docName);
+
+    ShowQueryResult(String docInfo) {
+        String [] docInfos= new String [6];
+        sp_Dipartment = new SimpleStringProperty(docInfos[0]);
+        sp_Course = new SimpleStringProperty(docInfos[1]);
+        sp_Type = new SimpleStringProperty(docInfos[2]);
+        sp_Year = new SimpleStringProperty(docInfos[3]);
+        sp_Lectuarer = new SimpleStringProperty(docInfos[4]);
+        sp_docLink = new SimpleStringProperty(docInfos[5]);
     }
 
-    public String getSp_docName() {
-        return sp_docName.get();
+    public StringProperty getDipartmentProperty() {
+        return sp_Dipartment;
     }
 
-    public StringProperty sp_docNameProperty() {
-        return sp_docName;
+
+    public StringProperty getCourseProperty() {
+        return sp_Course;
     }
+
+    public StringProperty getTypeProperty() {
+        return sp_Type;
+    }
+
+    public StringProperty getYearProperty() {
+        return sp_Year;
+    }
+
+    public StringProperty getLectuarerProperty() {
+        return sp_Lectuarer;
+    }
+
+
+    public StringProperty sp_docLinkProperty() {
+        return sp_docLink;
+    }
+
 }
