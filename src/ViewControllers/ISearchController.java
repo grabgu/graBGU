04c969460645662c3e333fdaba3c1b6c1c546a8d
hilakhadad <hilakhadad@gmail.com>
2019-03-28 17:31:27 +0200

    new isearchController

--- src/ViewControllers/ISearchController.java
+++ src/ViewControllers/ISearchController.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface ISearchController {
 
-    List<String> getRelevantCourseList();
+    List<String> getRelevantCoursesList();
 
     List<Integer> getRelevantYearsList();
 
